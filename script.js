f42216813c36cac2e7c07d1bc208bc272a13aa09
Shane Jeremich <shane.jeremich@gmail.com>
2020-12-22 15:42:59 -0700

    Added dummy transactions to style expense tracker, tracker total amount works, and totals for income and expenses work

---
+++ script.js
@@ -0,0 +1,58 @@
+const balance = document.getElementById("balance"),
+  money_plus = document.getElementById("money-plus"),
+  money_minus = document.getElementById("money-minus"),
+  list = document.getElementById("list"),
+  form = document.getElementById("form"),
+  text = document.getElementById("text"),
+  amount = document.getElementById("amount");
+
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
+    </span> <button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(amount => amount > 0)
+    .reduce((acc, amounts) => (acc += amounts), 0)
+    .toFixed(2);
+
+  const expense = (amounts.filter(amount => amount < 0).reduce((acc, amounts) => (acc -= amounts), 0) * -1).toFixed(2);
+
+  balance.innerHTML = `$${total}`;
+  money_plus.innerHTML = `$${income}`;
+  money_minus.innerHTML = `$${expense}`;
+}
+
+// Init app
+(function init() {
+  list.innerHTML = "";
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+})();
